d0d99265d9c0ed5292d5035ce4a913a094266d83
kom1lov001 <avazkomilov94@gmail.com>
2023-07-16 09:24:13 +0300

    first commit

---
+++ src/views/EnterView.vue
@@ -0,0 +1,13 @@
+<template >
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'EnterView',
+}
+</script>
+<style>
+    
+</style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,13 @@
+<template >
+    <div>
+        Register page
+    </div>
+</template>
+<script>
+export default {
+    name:'RegisterView',
+}
+</script>
+<style >
+    
+</style>
